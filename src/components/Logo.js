fe8fbe0f8b00f65899b396127caf7233cf468459
MattWagner28 <matthewwagner@outlook.com>
2024-05-14 11:54:52 -0400

    fixed Logo vid

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,18 +2,16 @@ import React from "react";
 
 function Logo() {
     return (
-        <div className='flex justify-center'>
-            
-                {/* <img className='logo' src='./logo.gif' alt='GIF of Extremely Pure Logo' width='644' height='362' /> */}
-                <video autoPlay loop muted width='644' height='362'>
-                <source src='logo.mp4' type='video/mp4' />
+        <div className='flex justify-center overflow-hidden'>
+            <video autoPlay loop muted width='644' height='362' style={{ clipPath: 'inset(1px 1px)' }}>
+                <source src='logoVid.mp4' type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
         </div>
-    )
-  }
-  
-  export default Logo;
+    );
+}
+
+export default Logo;
 
 
   
