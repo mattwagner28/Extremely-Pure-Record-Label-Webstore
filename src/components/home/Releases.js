81cac947edb41b705452273f2d42de78de3759ea
MattWagner28 <matthewwagner@outlook.com>
2024-05-14 14:48:47 -0400

    added hover interactivtiy to album art icons on homepage

--- src/components/home/Releases.js
+++ src/components/home/Releases.js
@@ -1,27 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import releasesData from '../../albumDetails.json';
 
 function Releases() {
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+
+    const albumInfoText = "uppercase font-semibold";
+
     return (
         <div className='w-2/5 border-solid border-2 m-2'>
             <h1 className="text-center font-semibold">RECENT RELEASES</h1>
-                <div className="grid grid-cols-2">
-                    {releasesData.releases.map((release, index) => (
-                        <div className='cover art p-1'key={index}>
-                            <div className="absolute">
-                                <h3>{release.artist}</h3>
-                                <p>{release.title}</p>
-                                <p>{release.type}</p>
-                                <p>{release.format}</p>
-                            </div>
-                            <img src={`/albumart/${release.image}`} alt={release.title} />
+            <div className="grid grid-cols-2">
+                {releasesData.releases.map((release, index) => (
+                    <div
+                        key={index}
+                        className='cover-art p-1 relative'
+                        onMouseEnter={() => setHoveredIndex(index)}
+                        onMouseLeave={() => setHoveredIndex(null)}
+                    >
+                        <div className={`text-over-art p-2 absolute ${hoveredIndex === index ? 'block' : 'hidden'}`}>
+                            <h3 className={albumInfoText}>{release.artist}</h3>
+                            <p>{release.title}</p>
+                            <p>{release.type}</p>
+                            <p>{release.format}</p>
                         </div>
+                        <img src={`/albumart/${release.image}`} alt={release.title} className={`${hoveredIndex === index ? 'opacity-25' : 'opacity-100'}`}/>
+                    </div>
                 ))}
-                </div>
-                
+            </div>
         </div>
-    )
+    );
 }
 
 export default Releases;
-
