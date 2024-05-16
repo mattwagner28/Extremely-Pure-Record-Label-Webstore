a2eae189d6ed4aaafdebf3cc54093ff12a0b7e13
MattWagner28 <matthewwagner@outlook.com>
2024-05-16 12:56:20 -0400

    added Coming Soon to homepage to deploy while I work on MVP

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,12 +1,13 @@
 import React from "react";
 // import News from "./News";
-import Releases from "./Releases";
+// import Releases from "./Releases";
 
 function Home() {
     return (
         <div className='w-full flex justify-center mt-10'>
+            <h1>COMING SOON!</h1>
             {/* <News /> */}
-            <Releases />
+            {/* <Releases /> */}
         </div>
     )
 }
