d0e60493ca8b39ccdb11841ca25f060b9a1b036a
MattWagner28 <matthewwagner@outlook.com>
2024-07-09 12:30:55 -0400

    fixed logo by overlapping image onto video

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -3,10 +3,12 @@ import React from "react";
 function Logo() {
     return (
         <div className='flex justify-center overflow-hidden'>
-            <video autoPlay loop muted width='644' height='362' style={{ clipPath: 'inset(1px 1px)' }}>
-                <source src='logoVid.mp4' type='video/mp4' />
+            
+            <video className="static" autoPlay loop muted width='644' height='362'>
+                <source src='15secVidNoLogo.mp4' type='video/mp4' />
                 Your browser does not support the video tag.
             </video>
+            <img className="absolute z-10" width='660' height='371' src='clearLogo.png' alt='extremely pure logo'></img>
         </div>
     );
 }
